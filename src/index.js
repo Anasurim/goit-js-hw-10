807f5e0b603cca8439192ac06c3e718e9f257f40
Lebedynskyi Roman <cliffcrystall@gmail.com>
2023-03-15 21:52:12 +0100

    homework 10 added default file s

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+fetch('https://restcountries.com/v3.1/independent?status=true').then(
+  response => {
+    return response.json();
+  }
+);
