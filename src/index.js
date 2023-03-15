2eda79d017ef76aecf8cfa5d239b9c164aabff91
Lebedynskyi Roman <cliffcrystall@gmail.com>
2023-03-15 21:55:49 +0100

    homework10: fetched data from Rest api

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
 
-fetch('https://restcountries.com/v3.1/independent?status=true').then(
-  response => {
+fetch('https://restcountries.com/v3.1/independent?status=true')
+  .then(response => {
     return response.json();
-  }
-);
+  })
+  .then(data => {
+    console.log(data);
+  });
