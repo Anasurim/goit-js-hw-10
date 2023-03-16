3ff1fb2c1644b71994d96a2df8962d63bce2c250
Lebedynskyi Roman <cliffcrystall@gmail.com>
2023-03-16 13:06:13 +0100

    homework10: added listener on input

--- src/index.js
+++ src/index.js
@@ -2,10 +2,23 @@ import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
 
-fetch('https://restcountries.com/v3.1/independent?status=true')
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-  });
+const refs = {
+  input: document.querySelector('#search-box'),
+};
+
+refs.input.addEventListener('input', e => {
+  const countryName = e.currentTarget.value;
+
+  fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+});
+
+function fetchCountries() {}
